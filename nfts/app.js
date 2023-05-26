98c374c499c20e2dc040262520862c9af0639c49
crownnatty1 <131195435+crownnatty1@users.noreply.github.com>
2023-05-26 19:57:40 +0100

    Add files via upload

---
+++ nfts/app.js
@@ -0,0 +1,22 @@
+// function loading() {
+//     const xhr = new XMLHttpRequest()
+
+//     xhr.open('GET', 'customer.JSON', true);
+
+//     xhr.onload = function() {
+//         if (this.status === 200) {
+//             console.log(this.responseText)
+//         }
+//     }
+
+//     xhr.send()
+// }
+
+// loading()
+
+// if (window.matchMedia("(min-width: 980px)")) {
+//     document.querySelector('.display-sideNav').style.display = 'none'
+// } else {
+//     // document.querySelector('.profile').style.width = '100%'
+//     // document.querySelector('.nav').style.width = '100%'
+// }
